cf3e6daf254d5db443b57113529dcb42512b0010
jamesliambutler <james.liam.butler@kainos.com>
2024-09-27 09:09:39 +0100

    Create delivery employee table

---
+++ src/main/resources/db/migration/V5__create_delivery_employee_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE deliveryEmployee (
+	delEmpId int NOT NULL AUTO_INCREMENT,
+    `name` varchar(50),
+    salary decimal(7, 2),
+    currency char(3),
+	bankAccountNumber varchar(30),
+    nationalInsurance varchar(30)
+);
