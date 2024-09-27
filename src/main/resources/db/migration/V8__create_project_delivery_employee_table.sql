a4cb8c06f5b54a7ee5072884f0e3c8fd3f43223a
dylanmckk <dylan.mckenzie@kainos.com>
2024-09-27 14:25:32 +0100

    JTD-027 Fixed naming, timestamp and made finish date nullable

--- src/main/resources/db/migration/V8__create_project_delivery_employee_table.sql
+++ src/main/resources/db/migration/V8__create_project_delivery_employee_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE projectDeliveryEmployees (
-    projectId int NOT NULL AUTO_INCREMENT,
-    deliveryEmployeeId int NOT NULL AUTO_INCREMENT,
-    startDate DateTime NOT NULL TIMESTAMP,
-    finishDate DateTime NOT NULL,
+    projectId int NOT NULL,
+    deliveryEmployeeId int NOT NULL,
+    employeeStartDate startDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    employeeFinishDate DateTime
     FOREIGN KEY(projectId) REFERENCES project(projectId),
     FOREIGN KEY(deliveryEmployeeId) REFERENCES deliveryEmployee(deliveryEmployeeId)
 );
