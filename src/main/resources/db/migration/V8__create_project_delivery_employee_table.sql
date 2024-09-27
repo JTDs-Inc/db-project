157295b6082c85e56182cfc21aa76c08cae4912d
dylanmckk <dylan.mckenzie@kainos.com>
2024-09-27 14:37:12 +0100

    JTD-027 Changed TIMESTAMP to DATETIME, more consistent

--- src/main/resources/db/migration/V8__create_project_delivery_employee_table.sql
+++ src/main/resources/db/migration/V8__create_project_delivery_employee_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE projectDeliveryEmployees (
     projectId int NOT NULL,
     deliveryEmployeeId int NOT NULL,
-    employeeStartDate startDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    employeeFinishDate DateTime
+    employeeStartDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    employeeFinishDate DATETIME,
     FOREIGN KEY(projectId) REFERENCES project(projectId),
     FOREIGN KEY(deliveryEmployeeId) REFERENCES deliveryEmployee(deliveryEmployeeId)
 );
